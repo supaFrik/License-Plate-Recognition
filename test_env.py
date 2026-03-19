47e85bba507b74184bde668705acbd89578b759e
supaFrik <tqv2005business@gmail.com>
2026-03-19 15:43:51 +0700

    Add files via upload

--- test_env.py
+++
@@ -1,9 +0,0 @@
-import torch
-import cv2
-import numpy as np
-from PIL import Image
-
-print("Torch:", torch.__version__)
-
-img = np.zeros((32,32,3), dtype=np.uint8)
-print("Image shape:", img.shape)
